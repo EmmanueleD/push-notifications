35b6fb02a79a95e0f36df04dc17b90d9dfc32595
EmmanueleD <emmanuele.online@gmail.com>
2022-11-07 17:19:48 -0300

    dwa

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,7 +1,26 @@
+import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
 import { onBackgroundMessage } from "firebase/messaging/sw";
 
-const messaging = getMessaging();
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAu8d1aof077dtxusuzpqe3HrPfAO1g3eI",
+  authDomain: "testing-firebase-notifications.firebaseapp.com",
+  projectId: "testing-firebase-notifications",
+  storageBucket: "testing-firebase-notifications.appspot.com",
+  messagingSenderId: "1054762658875",
+  appId: "1:1054762658875:web:17dc4595c98ba937cac01e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Cloud Messaging and get a reference to the service
+const messaging = getMessaging(app);
+
 onBackgroundMessage(messaging, (payload) => {
   console.log(
     "[firebase-messaging-sw.js] Received background message ",

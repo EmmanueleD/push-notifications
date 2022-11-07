8a4ca8378f8327a5ec86206e32633d02ce5b1ebe
EmmanueleD <emmanuele.online@gmail.com>
2022-11-07 17:50:44 -0300

    dwa

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -17,15 +17,4 @@ onBackgroundMessage(messaging, (payload) => {
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("../firebase-messaging-sw.js", { type: "module", scope: "__" })
-    .then(function (registration) {
-      console.log("Registration successful, scope is:", registration.scope);
-    })
-    .catch(function (err) {
-      console.log("Service worker registration failed, error:", err);
-    });
-}
-
 export default messaging;

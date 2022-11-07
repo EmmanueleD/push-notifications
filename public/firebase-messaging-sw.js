aba41526c4b75dfa76314e43d9c34dc964450faa
EmmanueleD <emmanuele.online@gmail.com>
2022-11-07 18:20:37 -0300

    dwa

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,9 @@
-importScripts("https://www.gstatic.com/firebasejs/8.2.7/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/8.2.7/firebase-messaging.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.13.0/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.13.0/firebase-messaging-compat.js"
+);
 
 var firebaseConfig = {
   apiKey: "AIzaSyAu8d1aof077dtxusuzpqe3HrPfAO1g3eI",

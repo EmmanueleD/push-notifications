56922b42d4fd572d32adbf5867ddeb9d4a5aefb7
EmmanueleD <emmanuele.online@gmail.com>
2022-11-07 18:10:28 -0300

    dwa

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,14 +7,14 @@ onBackgroundMessage(messaging, (payload) => {
     "[firebase-messaging-sw.js] Received background message ",
     payload
   );
-  // Customize notification here
-  const notificationTitle = "Background Message Title";
-  const notificationOptions = {
-    body: "Background Message body.",
-    icon: "/firebase-logo.png",
-  };
+  // // Customize notification here
+  // const notificationTitle = "Background Message Title";
+  // const notificationOptions = {
+  //   body: "Background Message body.",
+  //   icon: "/firebase-logo.png",
+  // };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  // self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
 export default messaging;
